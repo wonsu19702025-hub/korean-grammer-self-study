3064b7257d38c3135b04934408d4d957f3039c9a
wonsu19702025-hub <wonsu19702025@gmail.com>
2026-06-29 16:24:33 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Firebase 설정 파일
+// 이 파일은 .gitignore에 추가하거나 GitHub Secrets로 관리하세요
+window.__firebaseConfig = {
+  apiKey:            "AIzaSyDORsytgNeJguwLDQA3pGx9OSKDkm5bjFQ",
+  authDomain:        "korean-grammer-self-study.firebaseapp.com",
+  projectId:         "korean-grammer-self-study",
+  storageBucket:     "korean-grammer-self-study.firebasestorage.app",
+  messagingSenderId: "648789667517",
+  appId:             "1:648789667517:web:46815011ea7c01abbf11d4",
+  measurementId:     "G-QQHL11SN6P"
+};
